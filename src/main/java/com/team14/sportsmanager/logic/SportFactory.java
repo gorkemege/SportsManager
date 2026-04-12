46f8e6e8afd6b3feca5414329aa2daa3efcec6f4
muratsonmezz <154265629+muratsonmezz@users.noreply.github.com>
2026-04-12 22:17:28 +0300

    Implemented SportFactory.java

--- src/main/java/com/team14/sportsmanager/logic/SportFactory.java
+++ src/main/java/com/team14/sportsmanager/logic/SportFactory.java
@@ -1,4 +1,46 @@
-package com.team14.sportsmanager.logic;
+package com.team14.sportsmanager.logic; 
+
+import com.team14.sportsmanager.core.ISport;
+import com.team14.sportsmanager.core.ITeam;
+import com.team14.sportsmanager.model.HeadballPlayer;
+import com.team14.sportsmanager.model.HeadballSport;
+import com.team14.sportsmanager.model.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class SportFactory {
+
+    public static ISport createSport(String sportType) {
+        if ("HeadBall".equalsIgnoreCase(sportType) || "Headball".equalsIgnoreCase(sportType)) {
+            return new HeadballSport();
+        }
+        throw new IllegalArgumentException("Unknown sport type: " + sportType);
+    }
+
+    public static List<ITeam> createLeagueTeams(String sportType) {
+        List<ITeam> leagueTeams = new ArrayList<>();
+        Random rand = new Random();
+
+        if ("HeadBall".equalsIgnoreCase(sportType) || "Headball".equalsIgnoreCase(sportType)) {
+            for (int i = 1; i <= 20; i++) {
+                Team team = new Team("HeadBall FC " + i);
+           
+                for (int j = 1; j <= 10; j++) {
+                    int randomHeadPower = 50 + rand.nextInt(50); 
+                    int randomJumpHeight = 50 + rand.nextInt(50);
+                    
+                    HeadballPlayer player = new HeadballPlayer("Player " + i + "-" + j, randomHeadPower, randomJumpHeight);
+                    team.addPlayer(player);
+                }
+                
+                leagueTeams.add(team);
+            }
+        } else {
+            throw new IllegalArgumentException("Unknown sport type for teams: " + sportType);
+        }
+
+        return leagueTeams;
+    }
 }
