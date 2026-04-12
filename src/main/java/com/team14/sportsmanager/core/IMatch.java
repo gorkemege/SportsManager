a3436a8f5104a7f587873b62ba06d4487a52789c
mburakaras <mehmet.aras@std.izmirekonomi.edu.tr>
2026-04-12 21:10:03 +0300

    Iteam and IMatch interfaces

--- src/main/java/com/team14/sportsmanager/core/IMatch.java
+++ src/main/java/com/team14/sportsmanager/core/IMatch.java
@@ -1,4 +1,12 @@
 package com.team14.sportsmanager.core;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IMatch {
+    void simulatePeriod();
+    boolean isMatchFinished();
+    Map<ITeam, Integer> getCurrentScore();
+    void applyUserSubstitution(IPlayer outPlayer, IPlayer inPlayer);
+    List<String> getMatchEvents();
 }
--- src/main/java/com/team14/sportsmanager/core/ITeam.java
+++ src/main/java/com/team14/sportsmanager/core/ITeam.java
@@ -1,4 +1,9 @@
 package com.team14.sportsmanager.core;
 
-public interface ITeam {
+import java.util.List;
+
+public interface ITeam {String getTeamName();
+    List<IPlayer> getRoster();
+    void addMatchResult(int pointsEarned);
+    int getTotalPoints();
 }
