58d8cee204d5f88640cb196fdec1b99e58c68905
muratsonmezz <154265629+muratsonmezz@users.noreply.github.com>
2026-04-12 21:15:20 +0300

    Edited AbstractPlayer.java

--- src/main/java/com/team14/sportsmanager/core/AbstractPlayer.java
+++ src/main/java/com/team14/sportsmanager/core/AbstractPlayer.java
@@ -1,4 +1,17 @@
 package com.team14.sportsmanager.core;
+import java.util.HashMap;
+import java.util.Map;
 
-public abstract class AbstractPlayer implements IPlayer{
+public abstract class AbstractPlayer implements IPlayer {
+    protected String name;
+    protected boolean injured = false;
+    protected int injuryDuration = 0;
+    protected Map<String, Integer> attributes = new HashMap<>();
+
+    public AbstractPlayer(String name) { this.name = name; }
+    @Override public String getName() { return name; }
+    @Override public Map<String, Integer> getAttributes() { return attributes; }
+    @Override public boolean isInjured() { return injured; }
+    @Override public int getRemainingInjuryDuration() { return injuryDuration; }
+    @Override public void updateAttribute(String name, int val) { attributes.put(name, val); }
 }
