47247cee3231e9bd481dc4a1610ec373ef57b988
Görkem Ege Zor <egezor4@gmail.com>
2026-04-12 21:10:53 +0300

    Added IPlayer and ISport interfaces.

--- src/main/java/com/team14/sportsmanager/core/IPlayer.java
+++ src/main/java/com/team14/sportsmanager/core/IPlayer.java
@@ -1,4 +1,12 @@
 package com.team14.sportsmanager.core;
 
+import java.util.Comparator;
+import java.util.Map;
+
 public interface IPlayer {
+    String getName();
+    Map<String, Integer> getAttributes();
+    boolean isInjured();
+    int getRemainingInjuryDuration();
+    void updateAttribute(String attrName, int value);
 }
--- src/main/java/com/team14/sportsmanager/core/ISport.java
+++ src/main/java/com/team14/sportsmanager/core/ISport.java
@@ -1,5 +1,10 @@
 package com.team14.sportsmanager.core;
+import java.util.Comparator;
+import java.util.Map;
 
 public interface ISport {
-
+    String getSportName();
+    int getPeriodCount();
+    Comparator<ITeam> getStandingLogic();
+    Map<String, Integer> getScoringRules();
 }
