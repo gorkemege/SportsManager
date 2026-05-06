5fface89f4fb58ebc6ff911a4d22b0fb6a373664
mburakaras <mehmet.aras@std.izmirekonomi.edu.tr>
2026-05-06 12:56:38 +0300

    feat(ui): Add main page layout

--- src/main/java/com/team14/sportsmanager/ui/HelloApplication.java
+++ src/main/java/com/team14/sportsmanager/ui/HelloApplication.java
@@ -8,14 +8,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
 
+
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+
+        stage.setTitle("Sports Manager 2026");
+        stage.setScene(scene);
+        stage.setResizable(false); 
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/com/team14/sportsmanager/ui/HelloController.java
+++ src/main/java/com/team14/sportsmanager/ui/HelloController.java
@@ -1,14 +1,25 @@
 package com.team14.sportsmanager.ui;
-
+import com.team14.sportsmanager.logic.League;
+import com.team14.sportsmanager.logic.SportFactory;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private ComboBox<String> sportSelectionBox;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button startNewGameButton;
+    @FXML
+    public void initialize() {
+        sportSelectionBox.getItems().add("HeadBall");
+        sportSelectionBox.getSelectionModel().selectFirst();
+    }
+    @FXML
+    protected void onStartNewGameClick(ActionEvent event) {
+        String selectedSport = sportSelectionBox.getValue();
+        System.out.println("[SYSTEM] Selected Sport: " + selectedSport);
+        League newLeague = SportFactory.createLeague(selectedSport);
+        System.out.println("[SYSTEM] League successfully created! Total teams: " + newLeague.getStandings().size());
     }
 }
