099dc0505f3c121d0134b507edea88746777cd9b
mburakaras <mehmet.aras@std.izmirekonomi.edu.tr>
2026-05-08 16:50:59 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.team14.sportsmanager {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.xerial.sqlitejdbc;
+    requires java.sql;
 
     opens com.team14.sportsmanager.ui to javafx.fxml;
+
+    opens com.team14.sportsmanager.model to javafx.base;
+
     exports com.team14.sportsmanager.ui;
     exports com.team14.sportsmanager.core;
     exports com.team14.sportsmanager.logic;
